769c7ec6d95a271c2601e0f4453ca4783c91038f
leo <lmerkel@unistra.fr>
2023-12-01 11:07:12 +0100

    add EN traduction

--- exportgitlab/apps/export/views.py
+++ exportgitlab/apps/export/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 
 
 @login_required
@@ -16,10 +17,10 @@ def changetoken(request):
         form = UserForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
-            messages.add_message(request, messages.INFO, "Le token GitLab a bien été modifié")
+            messages.add_message(request, messages.INFO, _("Gitlab token modified"))
             return redirect("profile")
         else:
-            messages.add_message(request, messages.ERROR, "Le token GitLab est erroné")
+            messages.add_message(request, messages.ERROR, _("Wrong Gitlab token"))
     else:
         form = UserForm(instance=user)
     return render(request, "change_token_pending.html", {"form": form})

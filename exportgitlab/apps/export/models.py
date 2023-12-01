8693b9400a8138aae18dce97809aae953d6b2ae8
leo <lmerkel@unistra.fr>
2023-12-01 11:59:34 +0100

    create Project models

--- exportgitlab/apps/export/models.py
+++ exportgitlab/apps/export/models.py
@@ -3,10 +3,17 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
+class Project(models.Model):
+    gitlab_id = models.IntegerField(null=False)
+    name = models.CharField(null=False)
+    description = models.CharField(default=None, null=True, blank=True)
+    url = models.CharField(null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class User(AbstractUser):
     gitlab_token = models.CharField(
-        max_length=40, blank=True, default=None, help_text=_("Gitlab personal token"), null=True
-    )
+        max_length=40, blank=True, default=None, help_text=_("Gitlab personal token"), null=True)
+    projects = models.ManyToManyField(Project)

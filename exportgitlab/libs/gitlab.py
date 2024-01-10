e6ea3495c026072bf818c085abdbfdd3466381bc
leo <lmerkel@unistra.fr>
2024-01-10 16:01:25 +0100

    add accessibility color switch for labels. minor style changes

--- exportgitlab/libs/gitlab.py
+++ exportgitlab/libs/gitlab.py
@@ -1,5 +1,7 @@
 import gitlab
 
+from exportgitlab.libs.color_contrast import passes
+
 
 def gl_connection(user_token):
     gl = gitlab.Gitlab(url="https://git.unistra.fr", private_token=user_token)
@@ -9,7 +11,13 @@ def gl_connection(user_token):
 
 def get_labels_list(gitlab_project):
     gitlab_labels = gitlab_project.labels.list(get_all=True)
-    gitlab_labels_dict = {label.name: label.color for label in gitlab_labels}
+    gitlab_labels_dict = {}
+    for label in gitlab_labels:
+        accessibility_color_check = passes(label.color, "#ffffff")
+        if accessibility_color_check:
+            gitlab_labels_dict[label.name] = {"bg_color": label.color, "text_color": "#FFFFF"}
+        else:
+            gitlab_labels_dict[label.name] = {"bg_color": label.color, "text_color": "#000000"}
     return gitlab_labels_dict
 
 

e7488427841162285937a1e7d672f3bcd3dc065f
leo <lmerkel@unistra.fr>
2024-01-17 14:57:44 +0100

    add download issue interface enable/disable feature

--- exportgitlab/static/js/exportgitlab.js
+++ exportgitlab/static/js/exportgitlab.js
@@ -1,19 +1,48 @@
 const selectAll = document.querySelector("#selectAll")
 const span_check_on = document.getElementById("check_all_on");
 const span_check_off = document.getElementById("check_all_off");
-selectAll.addEventListener("click",function(){
-    console.log("Select All checkbox clicked.");
-    if(span_check_on.style.display === "none"){
+const ungroup_issue_radio = document.getElementById("ungroup_issue")
+const group_issue_radio = document.getElementById("group_issue")
+const download_button = document.getElementById("issue_dl_button")
+const checkboxes = document.querySelectorAll("#issue_form table tr input[type=checkbox]");
+selectAll.addEventListener("click", function () {
+    if (span_check_on.style.display === "none") {
         span_check_on.style.display = "block";
         span_check_off.style.display = "none";
-    }else{
+    } else {
         span_check_on.style.display = "none";
         span_check_off.style.display = "block";
     }
-    document.querySelector("#issue_form")
-        .querySelectorAll("table tr input[type=checkbox]")
-        .forEach(function (input){
-            input.checked=selectAll.checked
-        })
+    checkboxes.forEach(function (input) {
+        input.checked = selectAll.checked
+
+    })
+    updateStates();
 
 })
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("click", function () {
+        updateStates();
+    })
+})
+
+function updateStates() {
+    const checkedCount = document.querySelectorAll("#issue_form table tr input[type=checkbox]:checked").length;
+    switch (checkedCount) {
+        case 0:
+            ungroup_issue_radio.setAttribute("disabled", "disabled");
+            download_button.setAttribute("disabled", "disabled");
+            group_issue_radio.setAttribute("disabled", "disabled");
+            break;
+        case 1:
+            download_button.removeAttribute("disabled");
+            group_issue_radio.removeAttribute("disabled");
+            ungroup_issue_radio.setAttribute("disabled", "disabled");
+            break;
+        default:
+            ungroup_issue_radio.removeAttribute("disabled");
+            download_button.removeAttribute("disabled");
+            group_issue_radio.removeAttribute("disabled");
+            break;
+    }
+}
